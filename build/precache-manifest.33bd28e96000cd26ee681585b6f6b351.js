4188c26e4ccb82e637506e42f75ca2f82421b40f
pbrayer <skellocrusha@yahoo.com>
2019-06-10 20:35:03 -0500

    testing deploy

---
+++ build/precache-manifest.33bd28e96000cd26ee681585b6f6b351.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed83c42c3146a5bf098b9799dcfc0eed",
+    "url": "/ClickingGame/index.html"
+  },
+  {
+    "revision": "62870ee05dcdfae574b7",
+    "url": "/ClickingGame/static/css/main.7bd8bd54.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/ClickingGame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "62870ee05dcdfae574b7",
+    "url": "/ClickingGame/static/js/main.17cf3ecf.chunk.js"
+  },
+  {
+    "revision": "1f348f190c9a8af9f4b9",
+    "url": "/ClickingGame/static/js/runtime~main.bc925f9d.js"
+  }
+]);
