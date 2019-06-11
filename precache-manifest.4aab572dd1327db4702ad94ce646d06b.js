0d9641061643ba55c0aff49156ae0cd57b8e3631
pbrayer <skellocrusha@yahoo.com>
2019-06-10 20:27:20 -0500

    Updates

---
+++ precache-manifest.4aab572dd1327db4702ad94ce646d06b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b475b058e3f4980ea5565010f1ab9dd2",
+    "url": "/ClickingGame/index.html"
+  },
+  {
+    "revision": "4ec25be264020a9f2efe",
+    "url": "/ClickingGame/static/css/main.7bd8bd54.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/ClickingGame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "4ec25be264020a9f2efe",
+    "url": "/ClickingGame/static/js/main.400ea3c3.chunk.js"
+  },
+  {
+    "revision": "1f348f190c9a8af9f4b9",
+    "url": "/ClickingGame/static/js/runtime~main.bc925f9d.js"
+  }
+]);
